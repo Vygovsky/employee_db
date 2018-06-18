77689d1c1fa0abc665afe12118c18be1c8c2a618
Roman_v <vigmol@ukr.net>
2018-06-18 13:34:29 +0300

    JdbcEmployee add method findAll

--- src/main/java/ua/ukr/net/dao/JdbcEmployeeDao.java
+++ src/main/java/ua/ukr/net/dao/JdbcEmployeeDao.java
@@ -2,9 +2,14 @@ package ua.ukr.net.dao;
 
 import ua.ukr.net.model.Employee;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcEmployeeDao extends AbstractJdbcDao implements EmployeeDao {
+    private final String FIND_ALL = "SELECT* FROM EMPLOYEE";
 
     @Override
     public void update(Employee employee) {
@@ -23,7 +28,23 @@ public class JdbcEmployeeDao extends AbstractJdbcDao implements EmployeeDao {
 
     @Override
     public List<Employee> findAll() {
-        return null;
+        List<Employee> listEmployee = new ArrayList<>();
+        try {
+            Statement statement = createConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(FIND_ALL);
+
+            while (resultSet.next()) {
+                Employee employee = new Employee();
+                employee.setId(resultSet.getLong("ID"));
+                employee.setName(resultSet.getString("FIRST_NAME"));
+                employee.setEmail(resultSet.getString("EMAIL"));
+                employee.setBirthday(resultSet.getDate("BIRTHDAY"));
+                listEmployee.add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listEmployee;
     }
 
     @Override
